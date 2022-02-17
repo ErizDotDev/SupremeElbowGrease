f0ed3452771069b54a8b6d2a79b5628b521b22ac
Denzel dela Cruz <denzeldevwork01@gmail.com>
2022-02-18 06:42:00 +0800

    refactor (*): Apply strategy pattern to card transaction processes for regular and discounted cards

--- src/QLess.Infrastructure/Services/CardService.cs
+++ src/QLess.Infrastructure/Services/CardService.cs
@@ -1,80 +1,22 @@
 ﻿using QLess.Core.Domain;
 using QLess.Core.Enums;
 using QLess.Core.Interface;
+using QLess.Infrastructure.Processors;
 
 namespace QLess.Infrastructure.Services
 {
 	public class CardService : ICardService
 	{
+		private Dictionary<CardType, Func<BaseCardTransactionProcessor>> cardTransactionProcessorList = new Dictionary<CardType, Func<BaseCardTransactionProcessor>>()
+		{ 
+			{ CardType.Regular, () => new RegularCardTransactionProcessor() },
+			{ CardType.Discounted, () => new DiscountedCardTransactionProcessor() }
+		};
+
 		public async Task<CreateCardResponse> CreateCard(CardType cardType, decimal initialBalance, string specialIdNumber = "")
 		{
-			string cardNumber = string.Empty;
-
-			switch (cardType)
-			{
-				case CardType.Regular:
-					return await Task.FromResult(CreateRegularCard(initialBalance));
-				case CardType.Discounted:
-					return await Task.FromResult(CreateDiscountedCard(initialBalance, specialIdNumber));
-			}
-
-			return await Task.FromResult(new CreateCardResponse());
-		}
-
-		private CreateCardResponse CreateRegularCard(decimal initialBalance)
-		{
-			const int minimumRegularCardTypeLoad = 100;
-
-			if (initialBalance < minimumRegularCardTypeLoad)
-			{
-				return new CreateCardResponse
-				{
-					CardNumber = string.Empty,
-					ErrorMessage = $"Minimum initial load balance not reached. Please load your card with at least P{minimumRegularCardTypeLoad}.00"
-				};
-			}
-
-			string cardNumber = GenerateCardNumber();
-
-			return new CreateCardResponse { CardNumber = cardNumber };
-		}
-
-		private CreateCardResponse CreateDiscountedCard(decimal initialBalance, string specialIdNumber)
-		{
-			const int minimumDiscountedCardTypeLoad = 500;
-
-			if (initialBalance < minimumDiscountedCardTypeLoad)
-			{
-				return new CreateCardResponse
-				{ 
-					CardNumber = string.Empty,
-					ErrorMessage = $"Minimum initial load balance not reached. Please load your card with at least P{minimumDiscountedCardTypeLoad}.00"
-				};
-			}
-
-			if (!IsSpecialIdValid(specialIdNumber))
-			{
-				return new CreateCardResponse
-				{
-					CardNumber = string.Empty,
-					ErrorMessage = $"The provided ID number is invalid. Please provide a valid ID number. Valid ID numbers include the Senior Citizen control number and PWD ID number."
-				};
-			}
-
-			string cardNumber = GenerateCardNumber();
-
-			return new CreateCardResponse { CardNumber = cardNumber };
-		}
-
-		private string GenerateCardNumber()
-		{
-			return DateTime.Now.ToString("yyddMMhhmmss");
-		}
-
-		private bool IsSpecialIdValid(string specialIdNumber)
-		{
-			return !string.IsNullOrEmpty(specialIdNumber)
-				&& (specialIdNumber.Length == 10 || specialIdNumber.Length == 12);
+			var cardTransactionProcessor = cardTransactionProcessorList[cardType];
+			return await Task.FromResult(cardTransactionProcessor.Invoke().CreateCardDetails(initialBalance, specialIdNumber));
 		}
 	}
 }
