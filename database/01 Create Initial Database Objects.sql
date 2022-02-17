72adbf8e46636c93b631cfcc663d49b547df59d3
Denzel dela Cruz <denzeldevwork01@gmail.com>
2022-02-18 07:20:01 +0800

    refactor (database-scripts): Rename database table and column names to integrate with Dapper.Contrib library

--- database/01 Create Initial Database Objects.sql
+++ database/01 Create Initial Database Objects.sql
@@ -1,34 +1,45 @@
-﻿--CREATE DATABASE [qless]
---GO
+﻿CREATE DATABASE [qless]
+GO
 
---USE [qless]
---GO
+USE [qless]
+GO
 
-CREATE TABLE [card_types]
+CREATE TABLE [CardType]
 (
-	[id] INT PRIMARY KEY,
-	[description] NVARCHAR(50) NOT NULL
+	[Id] INT PRIMARY KEY,
+	[Description] NVARCHAR(50) NOT NULL
 )
 
-INSERT INTO [card_types] VALUES(1, 'Regular')
-INSERT INTO [card_types] VALUES(2, 'Discounted')
+INSERT INTO [CardType] VALUES(1, 'Regular')
+INSERT INTO [CardType] VALUES(2, 'Discounted')
 
-CREATE TABLE [cards]
+CREATE TABLE [CardDetail]
 (
-	[id] BIGINT PRIMARY KEY IDENTITY,
-	[card_type] INT FOREIGN KEY REFERENCES [card_types]([id]),
-	[card_number] NVARCHAR(15) NOT NULL,
-	[special_id_number] NVARCHAR(12) NULL,
-	[balance] DECIMAL NOT NULL,
-	[date_last_used] DATETIME NULL
+	[Id] BIGINT PRIMARY KEY IdENTITY,
+	[CardTypeId] INT FOREIGN KEY REFERENCES [CardType]([Id]),
+	[CardNumber] NVARCHAR(15) NOT NULL,
+	[SpecialIdNumber] NVARCHAR(12) NULL,
+	[Balance] DECIMAL NOT NULL,
+	[DateLastUsed] DATETIME NULL
 )
 
---CREATE TABLE [transactions]
---(
---	[id] BIGINT PRIMARY KEY IDENTITY,
---	[card_id] BIGINT FOREIGN KEY REFERENCES [cards]([id]),
---	[transaction_date] DATETIME NOT NULL,
---	[fare] DECIMAL NOT NULL,
---	[previous_balance] DECIMAL NOT NULL,
---	[new_balance] DECIMAL NOT NULL
---)
+CREATE TABLE [TransactionType]
+(
+	[Id] INT PRIMARY KEY,
+	[Description] NVARCHAR(50)
+)
+
+INSERT INTO [TransactionType] VALUES(1, 'Initial Load')
+INSERT INTO [TransactionType] VALUES(2, 'Pay Trip')
+INSERT INTO [TransactionType] VALUES(3, 'Reload Card')
+
+CREATE TABLE [Transaction]
+(
+	[Id] BIGINT PRIMARY KEY IdENTITY,
+	[CardId] BIGINT FOREIGN KEY REFERENCES [cards]([Id]),
+	[TransactionDate] DATETIME NOT NULL,
+	[TransactionTypeId] INT FOREIGN KEY REFERENCES [TransactionType]([Id]),
+	[TransactionAmount] DECIMAL NOT NULL,
+	[PreviousBalance] DECIMAL NOT NULL,
+	[NewBalance] DECIMAL NOT NULL
+)
