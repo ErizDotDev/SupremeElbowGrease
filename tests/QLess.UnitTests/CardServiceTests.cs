f64972f5b7e4ceb53caf5402462974911d02e2ca
Denzel dela Cruz <denzeldevwork01@gmail.com>
2022-02-17 10:40:47 +0800

    test (Core.CardService): Create unit tests for CreateCard method for discounted QLess transport card

--- tests/QLess.UnitTests/CardServiceTests.cs
+++ tests/QLess.UnitTests/CardServiceTests.cs
@@ -10,9 +10,9 @@ namespace QLess.UnitTests
 		private readonly ICardService _cardService = new CardService(); 
 
 		[Fact]
-		public async Task CreateCard_ReturnsCardNumber_GivenRegularCardTypeAndInitialBalance()
+		public async Task CreateCard_ReturnsCardNumber_GivenRegularCardTypeAndValidInitialBalance()
 		{
-			CardType cardType = CardType.Regular;
+			var cardType = CardType.Regular;
 			decimal initialBalance = 100m;
 
 			var result = await _cardService.CreateCard(cardType, initialBalance);
@@ -25,10 +25,63 @@ namespace QLess.UnitTests
 		[Fact]
 		public async Task CreateCard_ReturnsEmptyString_GivenRegularCardTypeAndInvalidInitialBalance()
 		{
-			CardType cardType = CardType.Regular;
-			decimal initialBalance = 0m;
+			var cardType = CardType.Regular;
+			decimal initialBalance = 99m;
 
 			var result = await _cardService.CreateCard(cardType, initialBalance);
+
+			Assert.True(string.IsNullOrEmpty(result.CardNumber));
+			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+		}
+
+		[Fact]
+		public async Task CreateCard_ReturnsCardNumber_GivenDiscountedCardTypeValidIDNumberAndValidInitialBalance()
+		{
+			var cardType = CardType.Discounted;
+			decimal initialBalance = 500m;
+			string specialIDNumber = "XXXXXXXXXX";
+
+			var result = await _cardService.CreateCard(cardType, initialBalance, specialIDNumber);
+
+			Assert.True(!string.IsNullOrEmpty(result.CardNumber));
+			Assert.Contains(DateTime.Now.ToString("yyddMM"), result.CardNumber);
+			Assert.True(string.IsNullOrEmpty(result.ErrorMessage));
+		}
+
+		[Fact]
+		public async Task CreateCard_ReturnsEmptyString_GivenDiscountedCardTypeInvalidIDNumberAndValidInitialBalance()
+		{
+			var cardType = CardType.Discounted;
+			decimal initialBalance = 500m;
+			string specialIDNumber = "XXXXXXXXXXX";
+
+			var result = await _cardService.CreateCard(cardType, initialBalance, specialIDNumber);
+
+			Assert.True(string.IsNullOrEmpty(result.CardNumber));
+			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+		}
+
+		[Fact]
+		public async Task CreateCard_ReturnsEmptyString_GivenDiscountedCardTypeBlankIDNumberAndValidInitialBalance()
+		{
+			var cardType = CardType.Discounted;
+			decimal initialBalance = 500m;
+			string specialIDNumber = "";
+
+			var result = await _cardService.CreateCard(cardType, initialBalance, specialIDNumber);
+
+			Assert.True(string.IsNullOrEmpty(result.CardNumber));
+			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+		}
+
+		[Fact]
+		public async Task CreateCard_ReturnsEmptyString_GivenDiscountedCardTypeValidIDNumberAndInvalidInitialBalance()
+		{
+			var cardType = CardType.Discounted;
+			decimal initialBalance = 499m;
+			string specialIDNumber = "XXXXXXXXXXXX";
+
+			var result = await _cardService.CreateCard(cardType, initialBalance, specialIDNumber);
 
 			Assert.True(string.IsNullOrEmpty(result.CardNumber));
 			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
