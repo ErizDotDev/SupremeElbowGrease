2e6ab5f11a6d0730a1dc7d465b13bf4b42956f79
Denzel dela Cruz <denzeldevwork01@gmail.com>
2022-02-18 10:07:31 +0800

    test (*): Reflect the refactored code of card transaction processors on the unit tests

---
+++ tests/QLess.Infrastructure.UnitTests/Processors/Card/DiscountedCardTransactionProcessorTests/TryCreateCardNumberShould.cs
@@ -0,0 +1,64 @@
+﻿using QLess.Infrastructure.Processors;
+using Xunit;
+
+namespace QLess.Infrastructure.UnitTests.Processors.Card.DiscountedCardTransactionProcessorTests
+{
+	public class TryCreateCardNumberShould
+	{
+		private readonly BaseCardTransactionProcessor cardTransactionProcessor;
+
+		public TryCreateCardNumberShould()
+		{
+			cardTransactionProcessor = new DiscountedCardTransactionProcessor();
+		}
+
+		[Fact]
+		public void ReturnCardNumber_GivenDiscountedCardTypeValidIDNumberAndValidInitialBalance()
+		{			
+			decimal initialBalance = 500m;
+			string specialIDNumber = "XXXXXXXXXX";
+
+			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance, specialIDNumber);
+
+			Assert.True(!string.IsNullOrEmpty(result.CardNumber));
+			Assert.Contains(DateTime.Now.ToString("yyddMM"), result.CardNumber);
+			Assert.True(string.IsNullOrEmpty(result.ErrorMessage));
+		}
+
+		[Fact]
+		public void ReturnEmptyString_GivenDiscountedCardTypeInvalidIDNumberAndValidInitialBalance()
+		{
+			decimal initialBalance = 500m;
+			string specialIDNumber = "XXXXXXXXXXX";
+
+			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance, specialIDNumber);
+
+			Assert.True(string.IsNullOrEmpty(result.CardNumber));
+			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+		}
+
+		[Fact]
+		public void ReturnEmptyString_GivenDiscountedCardTypeBlankIDNumberAndValidInitialBalance()
+		{
+			decimal initialBalance = 500m;
+			string specialIDNumber = "";
+
+			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance, specialIDNumber);
+
+			Assert.True(string.IsNullOrEmpty(result.CardNumber));
+			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+		}
+
+		[Fact]
+		public void ReturnEmptyString_GivenDiscountedCardTypeValidIDNumberAndInvalidInitialBalance()
+		{
+			decimal initialBalance = 499m;
+			string specialIDNumber = "XXXXXXXXXXXX";
+
+			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance, specialIDNumber);
+
+			Assert.True(string.IsNullOrEmpty(result.CardNumber));
+			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+		}
+	}
+}
---
+++ tests/QLess.Infrastructure.UnitTests/Processors/Card/RegularCardTransactionProcessorTests/TryCreateCardNumberShould.cs
@@ -0,0 +1,38 @@
+﻿using QLess.Infrastructure.Processors;
+using Xunit;
+
+namespace QLess.Infrastructure.UnitTests.Processors.Card.RegularCardTransactionProcessorTests
+{
+	public class TryCreateCardNumberShould
+	{
+		private readonly BaseCardTransactionProcessor cardTransactionProcessor;
+
+		public TryCreateCardNumberShould()
+		{
+			cardTransactionProcessor = new RegularCardTransactionProcessor();
+		}
+
+		[Fact]
+		public void ReturnCardNumber_GivenRegularCardTypeAndValidInitialBalance()
+		{
+			decimal initialBalance = 100m;
+
+			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance);
+
+			Assert.True(!string.IsNullOrEmpty(result.CardNumber));
+			Assert.Contains(DateTime.Now.ToString("yyddMM"), result.CardNumber);
+			Assert.True(string.IsNullOrEmpty(result.ErrorMessage));
+		}
+
+		[Fact]
+		public void ReturnsEmptyString_GivenRegularCardTypeAndInvalidInitialBalance()
+		{
+			decimal initialBalance = 99m;
+
+			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance);
+
+			Assert.True(string.IsNullOrEmpty(result.CardNumber));
+			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+		}
+	}
+}
