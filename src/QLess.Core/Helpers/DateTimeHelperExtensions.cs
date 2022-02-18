8fb81374f3cf4ca3158f3850937fc7b88eab9d17
Denzel dela Cruz <denzeldevwork01@gmail.com>
2022-02-19 07:17:49 +0800

    feat: Add datetime helpers for generating start of day and end of day times to a given date

---
+++ src/QLess.Core/Helpers/DateTimeHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace QLess.Core.Helpers
+{
+	public static class DateTimeHelperExtensions
+	{
+		public static DateTime GetDayStartDateTime(this DateTime targetDate)
+		{
+			return new DateTime(targetDate.Year, targetDate.Month,
+				targetDate.Day, 0, 0, 0);
+		}
+
+		public static DateTime GetDayEndDateTime(this DateTime targetDate)
+		{
+			return new DateTime(targetDate.Year, targetDate.Month,
+				targetDate.Day, 23, 59, 59);
+		}
+	}
+}
