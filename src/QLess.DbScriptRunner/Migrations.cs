bfbf0f5f9435a04bb298ebba48ddd534372380d2
Denzel dela Cruz <denzeldevwork01@gmail.com>
2022-02-18 08:16:59 +0800

    feat: Create a database script runner project

---
+++ src/QLess.DbScriptRunner/Migrations.cs
@@ -0,0 +1,61 @@
+﻿using DbUp;
+using DbUp.Helpers;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace QLess.DbScriptRunner
+{
+	public class Migrations
+	{
+		private string _connectionString;
+
+		public Migrations(IConfiguration configuration)
+		{
+			_connectionString = configuration.GetConnectionString("QLessDbConnection");
+		}
+
+		public bool RunScripts()
+		{
+			try
+			{
+				var upgrader = DeployChanges.To
+					.SqlDatabase(_connectionString)
+					.WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(),
+						(string s) => s.StartsWith("QLess.DbScriptRunner.Scripts.Script"))
+					.JournalToSqlTable("dbo", "_script_migration_")
+					.LogToConsole()
+					.Build();
+
+				var result = upgrader.PerformUpgrade();
+
+				return result.Successful;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public bool RunReset()
+		{
+			try
+			{
+				var resetScriptRunner = DeployChanges.To
+					.SqlDatabase(_connectionString)
+					.WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(),
+						(string s) => s.StartsWith("QLess.DbScriptRunner.Scripts.Reset"))
+					.JournalTo(new NullJournal())
+					.LogToConsole()
+					.Build();
+
+				var result = resetScriptRunner.PerformUpgrade();
+
+				return result.Successful;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ src/QLess.DbScriptRunner/Scripts/Script01 Create Initial Database Objects.sql
@@ -0,0 +1,45 @@
+﻿CREATE DATABASE [qless]
+GO
+
+USE [qless]
+GO
+
+CREATE TABLE [CardType]
+(
+	[Id] INT PRIMARY KEY,
+	[Description] NVARCHAR(50) NOT NULL
+)
+
+INSERT INTO [CardType] VALUES(1, 'Regular')
+INSERT INTO [CardType] VALUES(2, 'Discounted')
+
+CREATE TABLE [CardDetail]
+(
+	[Id] BIGINT PRIMARY KEY IdENTITY,
+	[CardTypeId] INT FOREIGN KEY REFERENCES [CardType]([Id]),
+	[CardNumber] NVARCHAR(15) NOT NULL,
+	[SpecialIdNumber] NVARCHAR(12) NULL,
+	[Balance] DECIMAL NOT NULL,
+	[DateLastUsed] DATETIME NULL
+)
+
+CREATE TABLE [TransactionType]
+(
+	[Id] INT PRIMARY KEY,
+	[Description] NVARCHAR(50)
+)
+
+INSERT INTO [TransactionType] VALUES(1, 'Initial Load')
+INSERT INTO [TransactionType] VALUES(2, 'Pay Trip')
+INSERT INTO [TransactionType] VALUES(3, 'Reload Card')
+
+CREATE TABLE [Transaction]
+(
+	[Id] BIGINT PRIMARY KEY IdENTITY,
+	[CardId] BIGINT FOREIGN KEY REFERENCES [cards]([Id]),
+	[TransactionDate] DATETIME NOT NULL,
+	[TransactionTypeId] INT FOREIGN KEY REFERENCES [TransactionType]([Id]),
+	[TransactionAmount] DECIMAL NOT NULL,
+	[PreviousBalance] DECIMAL NOT NULL,
+	[NewBalance] DECIMAL NOT NULL
+)
