9bec77638416a42ffe9d896df26cc243836a80d6
Denzel dela Cruz <denzeldevwork01@gmail.com>
2022-02-19 05:44:38 +0800

    feat: Add repository dependency registration provider

---
+++ src/QLess.Infrastructure/Data/RepositoryRegistrationProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using QLess.Core.Interface;
+
+namespace QLess.Infrastructure.Data
+{
+	public static class RepositoryRegistrationProvider
+	{
+		public static void AddDataRepositories(this IServiceCollection services)
+		{
+			services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+			services.AddScoped<ICardRepository, CardRepository>();
+		}
+	}
+}
