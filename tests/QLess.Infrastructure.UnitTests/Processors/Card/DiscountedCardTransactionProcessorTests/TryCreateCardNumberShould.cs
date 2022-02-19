9e0a37e425872940cf2a5f9b045b6378b7e8eb28
Denzel dela Cruz <denzeldevwork01@gmail.com>
2022-02-19 10:52:54 +0800

    test (CardProcessorTests): Add test cases for additional requirement regarding initial card load amount

--- tests/QLess.Infrastructure.UnitTests/Processors/Card/DiscountedCardTransactionProcessorTests/TryCreateCardNumberShould.cs
+++ tests/QLess.Infrastructure.UnitTests/Processors/Card/DiscountedCardTransactionProcessorTests/TryCreateCardNumberShould.cs
@@ -5,6 +5,9 @@ namespace QLess.Infrastructure.UnitTests.Processors.Card.DiscountedCardTransacti
 {
 	public class TryCreateCardNumberShould
 	{
+		private const decimal MinimumDiscountedCardTypeLoad = 500m;
+		private const decimal MaximumLoadTransactionAmount = 1000m;
+
 		private readonly BaseCardTransactionProcessor cardTransactionProcessor;
 
 		public TryCreateCardNumberShould()
@@ -30,11 +33,13 @@ namespace QLess.Infrastructure.UnitTests.Processors.Card.DiscountedCardTransacti
 		{
 			decimal initialBalance = 500m;
 			string specialIDNumber = "XXXXXXXXXXX";
+			string expectedMessage = "The provided ID number is invalid. Please provide a valid ID number. Valid ID numbers include yout Senior Citizen control number and your PWD ID number.";
 
 			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance, specialIDNumber);
 
 			Assert.True(string.IsNullOrEmpty(result.CardNumber));
 			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+			Assert.Equal(expectedMessage, result.ErrorMessage);
 		}
 
 		[Fact]
@@ -42,23 +47,54 @@ namespace QLess.Infrastructure.UnitTests.Processors.Card.DiscountedCardTransacti
 		{
 			decimal initialBalance = 500m;
 			string specialIDNumber = "";
+			string expectedMessage = "The provided ID number is invalid. Please provide a valid ID number. Valid ID numbers include yout Senior Citizen control number and your PWD ID number.";
 
 			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance, specialIDNumber);
 
 			Assert.True(string.IsNullOrEmpty(result.CardNumber));
 			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+			Assert.Equal(expectedMessage, result.ErrorMessage);
 		}
 
 		[Fact]
-		public void ReturnEmptyString_GivenDiscountedCardTypeValidIDNumberAndInvalidInitialBalance()
+		public void ReturnEmptyString_GivenDiscountedCardTypeValidIDNumberAndInitialBalanceLessThanMinLoadAmount()
 		{
 			decimal initialBalance = 499m;
 			string specialIDNumber = "XXXXXXXXXXXX";
+			string expectedMessage = $"Minimum initial load balance not reached. Please load your card with at least P{MinimumDiscountedCardTypeLoad}.00";
 
 			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance, specialIDNumber);
 
 			Assert.True(string.IsNullOrEmpty(result.CardNumber));
 			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+			Assert.Equal(expectedMessage, result.ErrorMessage);
+		}
+
+		[Fact]
+		public void ReturnEmptyString_GivenDiscountedCardTypeValidIDNumberAndInitialBalanceGreaterThanMaxLoadAmount()
+		{
+			decimal initialBalance = 1001m;
+			string specialIDNumber = "XXXXXXXXXXXX";
+			string expectedMessage = $"Exceeded max load amount per transaction P{MaximumLoadTransactionAmount}.00.";
+
+			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance, specialIDNumber);
+
+			Assert.True(string.IsNullOrEmpty(result.CardNumber));
+			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+			Assert.Equal(expectedMessage, result.ErrorMessage);
+		}
+
+		[Fact]
+		public void ReturnCardNumber_GivenDiscountedCardTypeValidIDNumberAndInitialBalanceBetweenMinAndMaxLoadAmount()
+		{
+			decimal initialBalance = 999m;
+			string specialIDNumber = "XXXXXXXXXX";
+
+			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance, specialIDNumber);
+
+			Assert.True(!string.IsNullOrEmpty(result.CardNumber));
+			Assert.Contains(DateTime.Now.ToString("yyddMM"), result.CardNumber);
+			Assert.True(string.IsNullOrEmpty(result.ErrorMessage));
 		}
 	}
 }
--- tests/QLess.Infrastructure.UnitTests/Processors/Card/RegularCardTransactionProcessorTests/TryCreateCardNumberShould.cs
+++ tests/QLess.Infrastructure.UnitTests/Processors/Card/RegularCardTransactionProcessorTests/TryCreateCardNumberShould.cs
@@ -5,6 +5,9 @@ namespace QLess.Infrastructure.UnitTests.Processors.Card.RegularCardTransactionP
 {
 	public class TryCreateCardNumberShould
 	{
+		private const decimal MinimumRegularCardTypeLoad = 100m;
+		private const decimal MaximumLoadTransactionAmount = 1000m;
+
 		private readonly BaseCardTransactionProcessor cardTransactionProcessor;
 
 		public TryCreateCardNumberShould()
@@ -25,14 +28,41 @@ namespace QLess.Infrastructure.UnitTests.Processors.Card.RegularCardTransactionP
 		}
 
 		[Fact]
-		public void ReturnsEmptyString_GivenRegularCardTypeAndInvalidInitialBalance()
+		public void ReturnsEmptyString_GivenRegularCardTypeAndInitialBalanceLessThanMinLoadAmount()
 		{
 			decimal initialBalance = 99m;
+			string expectedMessage = $"Minimum initial load balance not reached. Please load your card with at least P{MinimumRegularCardTypeLoad}.00";
 
 			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance);
 
 			Assert.True(string.IsNullOrEmpty(result.CardNumber));
 			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+			Assert.Equal(expectedMessage, result.ErrorMessage);
+		}
+
+		[Fact]
+		public void ReturnsEmptyString_GivenRegularCardTypeAndInitialBalanceGreaterThanMinLoadAmount()
+		{
+			decimal initialBalance = 1001m;
+			string expectedMessage = $"Exceeded max load amount per transaction P{MaximumLoadTransactionAmount}.00.";
+
+			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance);
+
+			Assert.True(string.IsNullOrEmpty(result.CardNumber));
+			Assert.True(!string.IsNullOrEmpty(result.ErrorMessage));
+			Assert.Equal(expectedMessage, result.ErrorMessage);
+		}
+
+		[Fact]
+		public void ReturnCardNumber_GivenRegularCardTypeAndInitialBalanceBetweenMinAndMaxLoadAmount()
+		{
+			decimal initialBalance = 750m;
+
+			var result = cardTransactionProcessor.TryCreateCardNumber(initialBalance);
+
+			Assert.True(!string.IsNullOrEmpty(result.CardNumber));
+			Assert.Contains(DateTime.Now.ToString("yyddMM"), result.CardNumber);
+			Assert.True(string.IsNullOrEmpty(result.ErrorMessage));
 		}
 	}
 }
