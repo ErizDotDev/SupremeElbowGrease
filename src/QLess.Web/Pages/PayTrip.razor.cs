c81e9f63f755bac13bdf5bfa9d0ef949398a991d
Denzel dela Cruz <denzeldevwork01@gmail.com>
2022-02-20 21:12:28 +0800

    refactor (PayTrip): Transfer code behind to another file

---
+++ src/QLess.Web/Pages/PayTrip.razor.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Components;
+using QLess.Core.Domain;
+using QLess.Web.Interfaces;
+
+namespace QLess.Web.Pages
+{
+	public partial class PayTrip
+    {
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        [Inject]
+        public IQLessClientService CardClientService { get; set; }
+
+        private TripPaymentResponse _apiResponse = new();
+        private string _cardNumber = string.Empty;
+        private string _message = string.Empty;
+        private bool _isBusy = false;
+        private bool _showAlert = false;
+        private bool _showReceipt = false;
+
+        private async Task Submit()
+        {
+            _isBusy = true;
+            _showAlert = false;
+            _showReceipt = false;
+            _apiResponse = await CardClientService.PayTrip(_cardNumber);
+            if (_apiResponse.Succeeded)
+            {
+                _showReceipt = true;
+            }
+            else
+            {
+                _showAlert = true;
+                _message = _apiResponse.Message;
+            }
+
+            _isBusy = false;
+        }
+
+        private void GoToMainMenu()
+        {
+            Navigation.NavigateTo("/");
+        }
+    }
+}
