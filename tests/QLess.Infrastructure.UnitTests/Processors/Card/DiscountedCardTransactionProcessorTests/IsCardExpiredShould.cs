8c3bed0d30fd8076f37eddc1276816e59a4c3867
Denzel dela Cruz <denzeldevwork01@gmail.com>
2022-02-20 11:26:08 +0800

    test (CardTransactionProcessorTests): Create test suites for the IsCardExpired method for both card transaction processors

---
+++ tests/QLess.Infrastructure.UnitTests/Processors/Card/DiscountedCardTransactionProcessorTests/IsCardExpiredShould.cs
@@ -0,0 +1,37 @@
+﻿using QLess.Infrastructure.Processors;
+using Xunit;
+
+namespace QLess.Infrastructure.UnitTests.Processors.Card.DiscountedCardTransactionProcessorTests
+{
+	public class IsCardExpiredShould
+	{
+		private readonly DiscountedCardTransactionProcessor cardTransactionProcessor;
+
+		public IsCardExpiredShould()
+		{
+			cardTransactionProcessor = new DiscountedCardTransactionProcessor();
+		}
+
+		[Fact]
+		public void ReturnFalse_GivenDateLastUsedIsWithinTheCardExpirationPeriod()
+		{
+			DateTime dateLastUsed = DateTime.Now;
+			DateTime dateUsed = DateTime.Now.AddYears(2);
+
+			bool result = cardTransactionProcessor.IsCardExpired(dateLastUsed, dateUsed);
+
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void ReturnTrue_GivenDateLastUsedIsExceededTheCardExpirationPeriod()
+		{
+			DateTime dateLastUsed = DateTime.Now;
+			DateTime dateUsed = DateTime.Now.AddYears(5).AddDays(1);
+
+			bool result = cardTransactionProcessor.IsCardExpired(dateLastUsed, dateUsed);
+
+			Assert.True(result);
+		}
+	}
+}
---
+++ tests/QLess.Infrastructure.UnitTests/Processors/Card/RegularCardTransactionProcessorTests/IsCardExpiredShould.cs
@@ -0,0 +1,37 @@
+﻿using QLess.Infrastructure.Processors;
+using Xunit;
+
+namespace QLess.Infrastructure.UnitTests.Processors.Card.RegularCardTransactionProcessorTests
+{
+	public class IsCardExpiredShould
+	{
+		private readonly DiscountedCardTransactionProcessor cardTransactionProcessor;
+
+		public IsCardExpiredShould()
+		{
+			cardTransactionProcessor = new DiscountedCardTransactionProcessor();
+		}
+
+		[Fact]
+		public void ReturnFalse_GivenDateLastUsedIsWithinTheCardExpirationPeriod()
+		{
+			DateTime dateLastUsed = DateTime.Now;
+			DateTime dateUsed = DateTime.Now.AddYears(2);
+
+			bool result = cardTransactionProcessor.IsCardExpired(dateLastUsed, dateUsed);
+
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void ReturnTrue_GivenDateLastUsedIsExceededTheCardExpirationPeriod()
+		{
+			DateTime dateLastUsed = DateTime.Now;
+			DateTime dateUsed = DateTime.Now.AddYears(3).AddDays(10);
+
+			bool result = cardTransactionProcessor.IsCardExpired(dateLastUsed, dateUsed);
+
+			Assert.True(result);
+		}
+	}
+}
